89e7bc44777e9196fcf662425b274d0268f29711
treby <treby@atelier-nodoka.net>
2015-05-06 17:59:33 +0900

    Heroku settings.

--- config/initializers/influxdb-rails.rb
+++ config/initializers/influxdb-rails.rb
@@ -2,7 +2,7 @@ InfluxDB::Rails.configure do |config|
   config.influxdb_database = ENV['BORDERBOT_INFLUXDB_DATABASE'] || "eventborders"
   config.influxdb_username = ENV['BORDERBOT_INFLUXDB_USERNAME'] || "atndk"
   config.influxdb_password = ENV['BORDERBOT_INFLUXDB_PASSWORD'] || "test"
-  config.influxdb_hosts    = ["localhost"]
+  config.influxdb_hosts    = ENV['BORDERBOT_INFLUXDB_HOST'].nil? ? ["localhost"] : [ENV['BORDERBOT_INFLUXDB_HOST']]
   config.influxdb_port     = ENV['BORDERBOT_INFLUXDB_PORT'] || 8086
 
   config.logger = Logger.new(STDERR)

5db840f9158a1711d0b752badb7fa68d94061eb1
treby <treby@atelier-nodoka.net>
2015-05-08 02:20:02 +0900

    Fix factory value.

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     event_type_id 1
     name '大成！プラチナスターライブ4TH'
     series_name nil
-    sequence(:started_at) { |n| Time.parse('2015-01-16 17:00:00 +0900') + ((n - 1) * 12).days }
-    sequence(:ended_at) { |n| Time.parse('2015-01-27 23:59:59 +0900') + ((n - 1) * 12).days }
+    sequence(:started_at) { |n| Time.parse('2020-01-16 17:00:00 +0900') + (n * 2).days }
+    ended_at { started_at.nil? ? Time.now : ( started_at + 1.day ) }
   end
 end

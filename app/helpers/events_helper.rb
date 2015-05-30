c38a439ce8a3287652fa0a1ed2be6b89ca75765d
treby <treby@atelier-nodoka.net>
2015-05-30 23:20:52 +0900

    Ready for ENJOY H@RMONY!! :tada::tada:

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -84,4 +84,92 @@ module EventsHelper
 
   raw graph
   end
+
+  def am_charts_hhp(dataset)
+    series = dataset.first.keys.select { |k| k.include? 'border_' }.map do |border|
+      idol_id = border.match(/\d+/).to_s.to_i
+      { 'valueAxis' => 'v1',
+        'bullet' => 'round',
+        'bulletBorderThickness' => 1,
+        'hideBulletsCount' => 30,
+        'title' => "#{Pro765.send(Rubimas::Idol.names[idol_id - 1]).name}",
+        'valueField' => border,
+        'balloonText' => "[[title]]:[[value]]pt",
+        'fillAlphas' =>  0,
+      }
+    end
+
+    graph=<<-EOJS
+<div id="chartdiv" style='width:100%; height: 500px;'></div>
+<script>
+  (function() {
+  var chartData = #{raw dataset.to_json};
+  var getCategoryLabel = function(category, dataItem, categoryAxis) {
+    var period = new Date(category * 1000);
+    var options = {
+      day: "numeric", hour: "2-digit", minute: "2-digit"
+    };
+
+    var label = period.toLocaleTimeString("ja-JP", options);
+    label += "(" + dataItem.reward_rank + "枠)";
+    return label;
+  };
+
+  var chart = AmCharts.makeChart("chartdiv", {
+    "type": "serial",
+    "theme": "light",
+    "pathToImages": "http://www.amcharts.com/lib/3/images/",
+    "legend": {
+      "useGraphSettings": true,
+      "valueAlign": "left"
+    },
+    "dataProvider": chartData,
+    "valueAxes": [{
+      "id":"v1",
+      "axisThickness": 2,
+      "gridAlpha": 0,
+      "axisAlpha": 1,
+      "position": "left"
+     }],
+    "graphs": #{raw series.to_json},
+    "chartScrollbar": {},
+    "chartCursor": {
+      "cursorPosition": "mouse"
+    },
+    "categoryField": "time",
+    "categoryAxis": {
+      "categoryFunction" : getCategoryLabel,
+      "axisColor": "#DADADA",
+      "minorGridEnabled": true,
+      "labelRotation": 45
+    },
+    "export": {
+      "enabled": true,
+      "position": "bottom-right",
+      "libs": {
+        "path": "http://www.amcharts.com/lib/3/plugins/export/libs/"
+      }
+    }
+  });
+
+  var zoom_start = 0;
+  var zoom_end = chart.dataProvider.length - 1;
+
+  var zoomChart = function() {
+    chart.zoomToIndexes(zoom_start, zoom_end);
+  };
+
+  var updateZoomIndex = function(e) {
+    zoom_start = e.startIndex;
+    zoom_end = e.endIndex;
+  };
+
+  chart.addListener("dataUpdated", zoomChart);
+  chart.addListener("zoomed", updateZoomIndex);
+  })();
+</script>
+  EOJS
+
+  raw graph
+  end
 end
--- app/models/event/border.rb
+++ app/models/event/border.rb
@@ -1,24 +1,32 @@
 class Event::Border
+  @@meta_columns = %w(time sequence_number updated_at)
+
   def initialize(event)
     raise ArgumentError unless event.instance_of? Event
     @event = event
     @series_name = event.series_name
   end
 
-  def rank_columns
-    return @rank_columns if @rank_columns.present?
+  def columns
+    return @columns if @columns.present?
 
     res = InfluxDB::Rails.client.query "SELECT * FROM \"#{@series_name}\" LIMIT 1;"
     series_datas = res.values
     target_series_data = series_datas.first
     raw_columns = target_series_data.first.keys
-    @rank_columns = raw_columns.select { |c| c.include? 'border_' }.sort { |a, b| a.match(/(\d+)/).to_s.to_i <=> b.match(/(\d+)/).to_s.to_i }
+
+    border_columns = raw_columns.select { |c| c.include? 'border_' }.sort { |a, b| a.match(/(\d+)/).to_s.to_i <=> b.match(/(\d+)/).to_s.to_i }
+    other_columns = raw_columns.select do |column|
+      !(column.include?('border_') || @@meta_columns.include?(column))
+    end
+
+    @columns = border_columns.concat other_columns
   end
 
   def progress
     return @progress if @progress.present?
 
-    select_target = rank_columns.map { |column| "MIN(#{column}) AS #{column}" }
+    select_target = columns.map { |column| "MIN(#{column}) AS #{column}" }
     @progress = InfluxDB::Rails.client.query "SELECT #{select_target.join(',')} FROM \"#{@series_name}\" GROUP BY time(30m) ORDER ASC;"
   end
 

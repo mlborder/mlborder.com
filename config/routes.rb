b4244495dc376adff70a805c15a646e2bced5019
treby <treby@atelier-nodoka.net>
2015-06-11 02:40:52 +0900

    Add ENJOY H@RMONY!! special page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'events#show'
   get 'events/latest', to: 'events#show'
+  get 'enjoy_harmony', to: 'home#enjoy_harmony'
   resources :events, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.

8f041ab9bbaa677f506a9133aee2e5eb52991ad9
treby <treby@atelier-nodoka.net>
2015-07-30 20:23:56 +0900

    Default time_zone is Tokyo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  around_action :effect_time_zone
+
+  def time_zone
+    'Tokyo'
+  end
+
+  def effect_time_zone(&block)
+    Time.use_zone(time_zone, &block) if block.present?
+  end
 end

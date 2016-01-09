71815bfb99d13bacadaa8d7178eaa0514705ed3e
treby <treby@atelier-nodoka.net>
2016-01-09 17:33:42 +0900

    Add user's role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :alarms
 
+  enum role: %i(role_none role_admin)
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]

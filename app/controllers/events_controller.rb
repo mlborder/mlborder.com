55030c6289e181b4dadd6a6bc538b8d2adffd85d
treby <treby@atelier-nodoka.net>
2016-01-13 11:39:11 +0900

    Display the event prizes.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: %i(edit update default_series_name)
 
   def index
-    @events = Event.includes(:final_borders).order(id: :desc).page(params[:page])
+    @events = Event.includes(:prizes).includes(:final_borders).order(id: :desc).page(params[:page])
   end
 
   def show
@@ -12,7 +12,7 @@ class EventsController < ApplicationController
     return redirect_to events_path if @event.nil?
     @latest_data = @event.border.latest if @event.has_border?
     @dataset = @event.border.dataset if @event.has_border?
-    @recent_events = Event.includes(:final_borders).send(@event.event_type.to_sym).border_available.order(started_at: :desc).limit(10)
+    @recent_events = Event.includes(:prizes).includes(:final_borders).send(@event.event_type.to_sym).border_available.order(started_at: :desc).limit(10)
 
     respond_to do |format|
       format.html

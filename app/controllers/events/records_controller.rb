2b98748757dd884be1eec1fd61eca062811fa580
treby <treby@atelier-nodoka.net>
2016-01-17 20:40:08 +0900

    Support XHR request for the event records.

--- app/controllers/events/records_controller.rb
+++ app/controllers/events/records_controller.rb
@@ -2,12 +2,29 @@ class Events::RecordsController < ApplicationController
   before_action :set_event
 
   def index
-    if @event.records_available?
-      @idol = Idol.find_by(id: params[:idol_id].to_i)
-      @page_num = [1, params[:page].to_i].max
-      @records = @event.records(@page_num, @idol.try(:id))
+    if request.xhr?
+      if @event.records_available?
+        @idol = Idol.find_by(id: params[:idol_id].to_i)
+        @page_num = [1, params[:page].to_i].max
+        @records = @event.records(@page_num, @idol.try(:id))
+
+        render json: @records.map(&:attributes)
+      else
+        render json: nil, status: :not_found
+      end
     else
-      redirect_to event_path(@event)
+      if @event.records_available?
+        @idol = Idol.find_by(id: params[:idol_id].to_i)
+        @page_num = [1, params[:page].to_i].max
+        @records = @event.records(@page_num, @idol.try(:id))
+
+        respond_to do |format|
+          format.html
+          format.json { render json: @records.map(&:attributes) }
+        end
+      else
+        redirect_to event_path(@event)
+      end
     end
   end
 

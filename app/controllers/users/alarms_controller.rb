c2a57d891369532887f925366663db91d72da987
treby <treby@atelier-nodoka.net>
2016-01-25 18:42:40 +0900

    Fix the redirect path.

--- app/controllers/users/alarms_controller.rb
+++ app/controllers/users/alarms_controller.rb
@@ -1,7 +1,7 @@
 class Users::AlarmsController < ApplicationController
   before_action :set_user
   def index
-    (redirect_to watch_path and return) unless current_user
+    (redirect_to alarm_path and return) unless current_user
     (redirect_to user_alarms_path(@user) and return) if @user.id != current_user.id
 
     @alarms = @user.alarms
@@ -13,7 +13,7 @@ class Users::AlarmsController < ApplicationController
   end
 
   def create
-    (redirect_to watch_path and return) unless current_user
+    (redirect_to alarm_path and return) unless current_user
     (redirect_to user_alarms_path(@user) and return) if @user.id != current_user.id
 
     alarm = @user.alarms.build(alarm_params).tap { |a| a.target = :target_border; a.status = :status_valid }

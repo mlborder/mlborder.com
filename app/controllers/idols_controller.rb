c0a052a5d8b50098f5086f185a830bf05516eb09
treby <treby@atelier-nodoka.net>
2016-02-28 23:19:06 +0900

    Support excludes attributes.

--- app/controllers/idols_controller.rb
+++ app/controllers/idols_controller.rb
@@ -5,7 +5,7 @@ class IdolsController < ApplicationController
 
   def show
     @idol = Idol.find(idol_params[:id])
-    @events = Event.includes(:prizes).includes(:final_borders).order(id: :desc).where(id: @idol.event_prizes.pluck(:event_id))
+    @events = Event.includes(:final_borders).order(id: :desc).where(id: @idol.event_prizes.pluck(:event_id))
   end
 
   private

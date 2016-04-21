1fde682530c0b3ebb935ac069c94c64d9198c8c3
treby <treby@atelier-nodoka.net>
2016-04-21 17:04:52 +0900

    Display the difference from previous.

--- app/controllers/dramacd_themes_controller.rb
+++ app/controllers/dramacd_themes_controller.rb
@@ -1,8 +1,10 @@
 class DramacdThemesController < ApplicationController
   def index
     @themes = DramacdTheme.all
-    progress = @themes.first.vote_progress.latest
-    @latest_vote = progress[:values]
-    @latest_updated = progress[:time]
+    latest, previous = @themes.first.vote_progress.with_previous
+    @previous_vote = previous[:values]
+    @latest_vote = latest[:values]
+    @previous_updated = previous[:time]
+    @latest_updated = latest[:time]
   end
 end
--- app/models/dramacd_theme/vote_progress.rb
+++ app/models/dramacd_theme/vote_progress.rb
@@ -25,4 +25,36 @@ class DramacdTheme::VoteProgress
 
     { time: Time.parse(recent_series_data['time']).in_time_zone('Tokyo'), values: values }
   end
+
+  def with_previous(target_time = Time.now, duration = 1.hour)
+    target_time = @theme.end_time + 1.second if @theme.end_time < target_time
+    str_time_to = target_time.to_i
+    str_time_from = (target_time - duration).to_i
+    ret = InfluxDB::Rails.client.query "SELECT * FROM \"#{@series_name}\" WHERE time <= #{str_time_to}s AND time >= #{str_time_from - 300}s"
+    values = ret.first['values'].reverse
+
+    latest_time = Time.parse(values.first['time'])
+    index = -1
+    values.each_with_index do |data, i|
+      next if (latest_time - Time.parse(data['time']) < duration)
+      index = i
+      break
+    end
+
+    [values.first, values[index]].map do |raw_data|
+      values = raw_data.select { |k, _| k.include? 'vote_' }.inject({}) do |h, (key, vote_cnt)|
+        theme_id, role_id, idol_id = key.split('_')[1..3].map(&:to_i)
+
+        h.deep_merge({
+          theme_id => {
+            role_id => {
+              idol_id => vote_cnt.to_i
+            }
+          }
+        })
+      end
+
+      { time: Time.parse(raw_data['time']).in_time_zone('Tokyo'), values: values }
+    end
+  end
 end

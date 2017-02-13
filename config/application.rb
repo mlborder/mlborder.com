5a7c8151dcf5ee8b91c9c31103a090d8cf74a7a3
Hiroaki Ninomiya <me@treby.info>
2017-02-13 23:53:08 +0900

    chore(config): remove `raise_in_transactional_callbacks` due to deprecation.

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,6 @@ module Mlborder
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
     config.i18n.available_locales = %i(en ja)
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.generators.template_engine = :slim
 
     config.generators do |g|

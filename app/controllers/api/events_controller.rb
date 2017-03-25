cc64ca83c19558b5d969c0db50d66d6930ce24de
Hiroaki Ninomiya <me@treby.info>
2017-03-25 15:13:36 +0900

    Revert "Preparing the evnet API for LINE bot"
    This reverts commit b5ba09a95fe19ab22ee42573d9e25e786f64a6bc.

--- app/controllers/api/events_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::EventsController < ApiController
-  def show
-    @event = params[:id] ? Event.find(params[:id]) : Event.border_available.last
-    if @event.has_border?
-      @border = @event.border.latest
-      @before_border = @event.border.border_with_velocity.last
-    end
-  end
-end
--- app/views/api/events/show.json.jbuilder
+++
@@ -1,7 +0,0 @@
-json.(@event, :id, :name, :started_at, :ended_at, :event_type)
-json.is_in_session @event.in_session?
-if @border
-  json.border @border[:borders]
-  json.before_border @before_border
-  json.border_updated_at @border[:time]
-end
